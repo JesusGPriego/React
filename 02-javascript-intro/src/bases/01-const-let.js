b7a48ae294a43959e79f77450bd45c3060f0e6e3
JesusGPriego <suleware@gmail.com>
2022-01-01 22:25:53 +0100

    02-javascript-intro added

---
+++ 02-javascript-intro/src/bases/01-const-let.js
@@ -0,0 +1,13 @@
+//Var & const:
+
+const name = "Jesús";
+const surname = "García";
+
+let valorDado = 5;
+
+if (true) {
+  let valorDado = 3;
+  console.log(valorDado);
+}
+
+console.log(name, surname, valorDado);
