b8e5dd2d53b816ff20df28d63ace98fd429ea7e2
JesusGPriego <suleware@gmail.com>
2022-01-06 12:05:33 +0100

    05-gif-expert-app added

---
+++ 05-gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+//rafcp
+export const GifExpertApp = (props) => {
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+      <hr />
+    </>
+  );
+};
+
+GifExpertApp.propTypes = {};
